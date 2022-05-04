e4b9089a1954b68d5c022485dc25310f9058f395
kfly8 <kfly@cpan.org>
2022-05-04 15:56:58 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Plack::Middleware::SecureHeaders - It's new $module
+
+# SYNOPSIS
+
+    use Plack::Middleware::SecureHeaders;
+
+# DESCRIPTION
+
+Plack::Middleware::SecureHeaders is ...
+
+# LICENSE
+
+Copyright (C) kfly8.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kfly8 <kfly@cpan.org>
---
+++ lib/Plack/Middleware/SecureHeaders.pm
@@ -0,0 +1,39 @@
+package Plack::Middleware::SecureHeaders;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Middleware::SecureHeaders - It's new $module
+
+=head1 SYNOPSIS
+
+    use Plack::Middleware::SecureHeaders;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::SecureHeaders is ...
+
+=head1 LICENSE
+
+Copyright (C) kfly8.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kfly8 E<lt>kfly@cpan.orgE<gt>
+
+=cut
+
